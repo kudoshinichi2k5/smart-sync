d11fca07340ab0e4e01cdebfd4bca614adc6f1c7
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2026-07-05 22:40:36 +0700

    feat: upload markdown documents to OpenAI vector store

--- uploader/upload_vector_store.py
+++ uploader/upload_vector_store.py
@@ -1,21 +1,64 @@
+import json
 from pathlib import Path
-
 from openai import OpenAI
-
 from config import DOCS_DIR
 from config import OPENAI_API_KEY
+from config import BASE_DIR
 
-client = OpenAI(
-    api_key=OPENAI_API_KEY
-)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
-files = sorted(Path(DOCS_DIR).glob("*.md"))
+STATE_DIR = BASE_DIR / "state"
+STATE_DIR.mkdir(exist_ok=True)
+STATE_FILE = STATE_DIR / "vector_store.json"
 
-print("=" * 50)
-print("OpenAI Connected")
-print("=" * 50)
+def create_vector_store():
+    print("Creating Vector Store...")
 
-print(f"Found {len(files)} markdown files.\n")
+    store = client.vector_stores.create(
+        name="OptiSigns Knowledge Base"
+    )
+    print(f"Vector Store ID: {store.id}")
 
-for file in files:
-    print(file.name)
+    return store.id
+
+
+def upload_all_files(vector_store_id):
+    markdown_files = sorted(DOCS_DIR.glob("*.md"))
+
+    streams = [
+        open(file, "rb")
+        for file in markdown_files
+    ]
+
+    print(f"\nUploading {len(streams)} markdown files...")
+
+    batch = client.vector_stores.file_batches.upload_and_poll(
+        vector_store_id=vector_store_id,
+        files=streams
+    )
+
+    print("\nUpload Finished")
+    print(batch.status)
+    print(batch.file_counts)
+
+    for s in streams:
+        s.close()
+
+
+def save_state(vector_store_id):
+    with open(STATE_FILE, "w") as f:
+        json.dump(
+            {
+                "vector_store_id": vector_store_id
+            },
+            f,
+            indent=4,
+        )
+
+
+if __name__ == "__main__":
+    vector_store_id = create_vector_store()
+    upload_all_files(vector_store_id)
+    save_state(vector_store_id)
+
+    print("\nDone.")
